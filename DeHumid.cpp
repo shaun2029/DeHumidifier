28f6f554ade850a05d28b2b9000b9c6f452c116e
Shaun2029 <shauns2029@gmail.com>
2016-10-31 22:31:09 +0000

    Licence updates.

--- DeHumid.cpp
+++ DeHumid.cpp
@@ -1,10 +1,25 @@
-/* bcm2835.h
+/* DeHumid.cpp
   
-   Dehumidifier control using BME280 and the 
-   Energinie ENER314 RASPBERRY P I RF- TRANSMITTER BOARD
-  
-   Author: Shaun Simpson
-   Copyright (C) 2016 Shaun Simpson
+    Dehumidifier control using BME280 and the 
+    Energinie ENER314 RASPBERRY P I RF- TRANSMITTER BOARD
+
+    Author: Shaun Simpson
+    Copyright (C) 2016 Shaun Simpson
+   
+    This file is part of DeHumidifier.
+
+    DeHumidifier is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DeHumidifier is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DeHumidifier.  If not, see <http://www.gnu.org/licenses/>.   
 */
 
 
--- LICENSE.md
+++ LICENSE.md
@@ -1,3 +1,25 @@
+AdaFruit BME280 Licence:
+
+  This is a library for the BME280 humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME280 Breakout
+  ----> http://www.adafruit.com/products/2650
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+
+
+-------------------------------------------------------------------------------
+
+DeHumidifier Licence:
+
                     GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
